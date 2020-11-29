6875c046200766de6dfad22db88ecc6ee58831c8
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2020-11-29 02:10:06 -0300

    delecao dos arquivos anteriores

--- form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
-from wtforms.validators import DataRequired
-
-class NameForm(FlaskForm):
-    name = StringField('Whats is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
