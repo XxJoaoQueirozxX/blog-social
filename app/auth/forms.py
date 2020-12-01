d94871914805097d091fc219243c1fa73f6a2857
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2020-12-01 14:22:47 -0300

    fix - Falta de retorno na função user_loader causando sempre usuarios anonimos apos o login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Email, DataRequired, Length
+from wtforms.validators import Email, DataRequired, Length, Regexp, EqualTo, ValidationError
 
-class Loginform(FlaskForm):
-    email = StringField('Email', validators=[Email(), DataRequired(), Length(1,64)])
+from ..models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letter, numbers, dots or undercores')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message="Passwords must match.")])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("Email already registered.")
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("Username already in use.")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,23 +1,20 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, session
 from flask_login import login_user, logout_user, login_required
 from . import auth
-from ..models import User
-from .forms import Loginform
+from ..models import User, db
+from .forms import LoginForm, RegisterForm
+from datetime import timedelta
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = Loginform()
+    form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-
-        if (user is not None) and (user.verify_password(form.password.data)):
-            login_user(user, form.remember_me.data)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-                next = url_for('main.index')
-            return redirect(next)
-        flash('Invalid username or password.')
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, remember = True, duration = timedelta(days=365), force=True)
+            return redirect(url_for('main.index'))
+        flash('Invalid email or password.')
     return render_template('auth/login.html', form=form)
 
 
@@ -27,3 +24,19 @@ def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("You can now login.")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
