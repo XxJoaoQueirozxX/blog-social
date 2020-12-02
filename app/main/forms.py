a28ed602e7ddddb8306a11870e1a9ab70f4c9ed8
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2020-12-02 19:27:49 -0300

    add- funcao de edicao de perfil

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
 
 class NameForm(FlaskForm):
     name = StringField('Whats is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 from ..decorators import permission_required, admin_required, Permission
 from . import main
-from ..models import User
+from ..models import User, db
+from .forms import EditProfileForm
 
 @main.route('/', methods=["GET", "POST"])
 def index():
@@ -28,8 +29,25 @@ def admin():
 def moderate():
     return "For moderate only"
 
+
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template("user.html", user=user)
 
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me = current_user.about_me
+    return render_template('edit_profile.html', form=form)
