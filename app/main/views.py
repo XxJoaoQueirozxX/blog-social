4c74452469dc474cd68535fd061839b45c048b23
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2020-12-02 18:48:56 -0300

    add - Pagina de Perfil do usuario

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask_login import login_required
 from ..decorators import permission_required, admin_required, Permission
 from . import main
-
+from ..models import User
 
 @main.route('/', methods=["GET", "POST"])
 def index():
@@ -27,4 +27,9 @@ def admin():
 @permission_required(Permission.MODERATE)
 def moderate():
     return "For moderate only"
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template("user.html", user=user)
 
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_login import UserMixin, AnonymousUserMixin
 from flask import current_app
+from datetime import datetime
 
 from . import db, login_manager
 
@@ -68,6 +69,12 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
     confirmed = db.Column(db.Boolean, default=False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
@@ -92,6 +99,11 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
 
     def get_id(self):
         return str(self.id)
