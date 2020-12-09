fe985b1826b14f02121aacb343bda584ee3d1f92
XxJoaoQueirozxX <joaovic.pqueiroz@gmail.com>
2020-12-08 23:27:25 -0300

    fix - handler de erro 404 refatorado para devolver resposta no formato json para apis

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 from . import main
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
 
 
 @main.app_errorhandler(500)
